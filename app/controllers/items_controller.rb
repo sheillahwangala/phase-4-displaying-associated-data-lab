5f1c8d3406c31122cebf2f82dabbd4f79b037a03
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-27 13:34:10 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all 
+        render json: items, include: :user, except:[:ceated_at, :updated_at]
+    end
+    
 end
